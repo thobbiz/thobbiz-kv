1ca5616262b5d911aa3fe4896caa013cbcb971a3
thobbiz <odelolatojumi@gmail.com>
2026-01-27 06:20:57 +0100

    feat: defined models for store and records

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thobbiz/thobbixDB
+
+go 1.25.5
---
+++ models.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"sync"
+)
+
+type Store struct {
+	filename string
+	file     *os.File
+	mu       sync.RWMutex
+	index    map[string]int64
+}
+
+type Record struct {
+	Key       []byte
+	Value     []byte
+	Timestamp uint32
+	ExpiresAt uint64
+	TombStone bool
+}
